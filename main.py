90b9facc24c82bef18d949c2187cfa4e1adfcfa5
Devang Sampat <devangs27@gmail.com>
2013-02-16 15:55:10 -0800

    Updating ISA to reflect changes we made to it and outputting to format we want

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import sys
+
 def parse(line):
-    instruction, target = line.split(' ')
+    instruction, target = line.replace('\n', '').split(' ')
     if instruction not in instructions:
         raise ParseError(instruction + ' is not a valid instruction')
     else:
-        operation = instructions['instruction']
+        operation = instructions[instruction]
         if operation[0] is 'r':
             return operation[1] + registers[target]
         elif operation[0] is 'i':
@@ -17,8 +19,8 @@ def assemble_file(input_file_location, output_file_location):
     output_file = open(output_file_location, 'w')
     for line_number, line in enumerate(input_file):
         try:
-            result = parse(line)
-            output_file.writeLine(result)  # Append '\n'?
+            output = '{0}: instruction = 9\'b{1};\n'.format(line_number, parse(line))
+            output_file.write(output)
         except ParseError as e:
             print line_number, e.message
     input_file.close()
@@ -33,31 +35,31 @@ class ParseError(Exception):
         return repr(self.message)
 
 instructions = {
-    'setTar': ('r', '00000', 'TAR_REG'),
-    'addTar': ('i', '00001', 't'),
-    'subTar': ('i', '00010', 't'),
-    'shiftR': ('i', '00011', 't'),
-    'shiftL': ('i', '00100', 't'),
-    'copyTo': ('r', '00110', 't'),
-    'copyFr': ('r', '00111', 't'),
-    'addReg': ('r', '01000', 't'),
-    'subReg': ('r', '01001', 't'),
-    'difReg': ('r', '01010', 't'),
-    'andAsn': ('r', '01011', 't'),
-    'strTar': ('r', '01100', 't'),
-    'lodMem': ('r', '01101', 't'),
-    'compLT': ('r', '01110', 'LT_REG'),
-    'compGT': ('r', '01111', 'GT_REG'),
-    'compEQ': ('r', '10000', 'EQ_REG'),
-    'compNE': ('r', '10001', 'NE_REG'),
+    'copyTo': ('r', '00010', 't'),
+    'copyFr': ('r', '00011', 't'),
+    'strTar': ('r', '00101', 't'),
+    'lodMem': ('r', '00110', 't'),
+    'addReg': ('r', '00000', 't'),
+    'subReg': ('r', '00001', 't'),
+    'andAsn': ('r', '00100', 't'),
+    'setTar': ('r', '01000', 'TAR_REG'),
+    'difReg': ('r', '01011', 't'),
+    'compLT': ('r', '01100', 'LT_REG'),
+    'compGT': ('r', '01101', 'GT_REG'),
+    'compEQ': ('r', '01110', 'EQ_REG'),
+    'compNE': ('r', '01111', 'NE_REG'),
+    'addTar': ('i', '10000', 't'),
+    'subTar': ('i', '10001', 't'),
+    'shiftL': ('i', '10010', 't'),
+    'shiftR': ('i', '10011', 't'),
     'setSec': ('i', '10010', None),
-    'clrReg': ('r', '10011', None),
-    'incReg': ('r', '10100', None),
-    'decReg': ('r', '10101', None),
-    'ggnore': ('', '10110', None),
-    'TBD': ('', '10111', None),
-    'jmpCnd': ('i', '11100', None),
-    'jmpAlw': ('i', '11110', None)
+    'ggnore': ('', '11000', None),
+    'jmpCnd': ('i', '11001', None),
+    'jmpAlw': ('i', '11010', None),
+    'TBD': ('', '11011', None),
+    'clrReg': ('r', '11101', None),
+    'decReg': ('r', '11110', None),
+    'incReg': ('r', '11111', None)
 }
 
 registers = {
@@ -69,11 +71,15 @@ registers = {
 '$5': '0101',
 '$6': '0111',
 '$7': '0111',
-'TAR_REG': '1000',
-'SEC_REG': '1001',
+'EQ_REG': '1000',
+'NE_REG': '1001',
 'DIFF_REG': '1011',
 'LT_REG': '1100',
 'GT_REG': '1101',
-'EQ_REG': '1110',
-'NE_REG': '1111'
+'TAR_REG': '1110',
+'SEC_REG': '1111'
 }
+
+if  __name__ =='__main__':
+    assemble_file(sys.argv[1], sys.argv[2])
+
